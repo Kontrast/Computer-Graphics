8190db1ea3f92bf346673b263c3f9ddd5c6e772c
Kontrast <ja.kontrast@gmail.com>
2013-10-24 13:04:54 +0300

    Separate parts of crane rendered

--- CrashedHope/CrashedHopeWPF/MainWindow.xaml.cs
+++ CrashedHope/CrashedHopeWPF/MainWindow.xaml.cs
@@ -26,10 +26,13 @@ namespace CrashedHopeWPF
         private Model boomModel;
         private Model hookModel;
         private Model platformModel;
-        float[] vertices;
-        uint[] indices;
+        List<float[]> vertices = new List<float[]>(4);
+        List<uint[]> indices = new List<uint[]>(4);
+        private float[] normals;
+
+        
         // used for storing the id of the vbo
-        uint[] vertexBufferObjectIds = new uint[1];
+        uint[] vertexBufferObjectIds = new uint[4];
         float rotation = 0;
 
         public MainWindow()
@@ -58,14 +61,24 @@ namespace CrashedHopeWPF
             gl.Rotate(0, rotation++, rotation / 2);
 
 
-            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, vertexBufferObjectIds[0]);
+            DrawBuffers(0, args);
+            DrawBuffers(1, args);
+            DrawBuffers(2, args);
+            DrawBuffers(3, args);
+        }
+
+        private void DrawBuffers(int bufferNum, OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+           
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, vertexBufferObjectIds[bufferNum]);
             gl.EnableClientState(OpenGL.GL_VERTEX_ARRAY);
             gl.EnableVertexAttribArray(0);
             gl.VertexAttribPointer(0, 3, OpenGL.GL_FLOAT, false, 0, new IntPtr(0));
 
-            gl.DrawElements(OpenGL.GL_TRIANGLES, indices.Length, indices);
+            gl.DrawElements(OpenGL.GL_TRIANGLES, indices.ElementAt(bufferNum).Length, indices.ElementAt(bufferNum));
         }
-        
+
         /// <summary>
         /// Handles the OpenGLInitialized event of the OpenGLControl control.
         /// </summary>
@@ -79,13 +92,17 @@ namespace CrashedHopeWPF
             hookModel = modelLoader.LoadModel(@"..\..\Resources\Tower crane_hook.obj");
             platformModel = modelLoader.LoadModel(@"..\..\Resources\Tower crane_platform.obj");
 
-            AddBuffer(towerModel, args);
-            AddBuffer(boomModel, args);
-            AddBuffer(hookModel, args);
-            AddBuffer(platformModel, args);
+            OpenGL gl = args.OpenGL;
+
+            gl.GenBuffers(4, vertexBufferObjectIds);
+
+            AddBuffer(towerModel, args, 0);
+            AddBuffer(boomModel, args, 1);
+            AddBuffer(hookModel, args, 2);
+            AddBuffer(platformModel, args, 3);
         }
 
-        private void AddBuffer(Model model, OpenGLEventArgs args)
+        private void AddBuffer(Model model, OpenGLEventArgs args, int bufferNum)
         {
             OpenGL gl = args.OpenGL;
 
@@ -94,35 +111,35 @@ namespace CrashedHopeWPF
 
             gl.Hint(OpenGL.GL_PERSPECTIVE_CORRECTION_HINT, OpenGL.GL_NICEST);
 
-            vertices = new float[model.Data.Vertices.Count() * 3];
-            indices = new uint[model.Data.Tris.Count() * 3];
+            vertices.Add(new float[model.Data.Vertices.Count() * 3]);
+            indices.Add(new uint[model.Data.Tris.Count() * 3]);
+            normals = new float[model.Data.Normals.Count() * 3];
 
             int i = 0;
             foreach (var vertice in model.Data.Vertices)
             {
-                vertices[i] = (float)vertice.X;
-                vertices[i + 1] = (float)vertice.Y;
-                vertices[i + 2] = (float)vertice.Z;
+                vertices.ElementAt(bufferNum)[i] = (float)vertice.X;
+                vertices.ElementAt(bufferNum)[i + 1] = (float)vertice.Y;
+                vertices.ElementAt(bufferNum)[i + 2] = (float)vertice.Z;
                 i += 3;
             }
             i = 0;
             foreach (var ind in model.Data.Tris)
             {
-                indices[i] = (uint)ind.P1.Vertex;
-                indices[i + 1] = (uint)ind.P2.Vertex;
-                indices[i + 2] = (uint)ind.P3.Vertex;
+                indices.ElementAt(bufferNum)[i] = (uint)ind.P1.Vertex;
+                indices.ElementAt(bufferNum)[i + 1] = (uint)ind.P2.Vertex;
+                indices.ElementAt(bufferNum)[i + 2] = (uint)ind.P3.Vertex;
                 i += 3;
             }
 
-            gl.GenBuffers(1, vertexBufferObjectIds);
-            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, vertexBufferObjectIds[0]);
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, vertexBufferObjectIds[bufferNum]);
 
             unsafe
             {
-                fixed (float* verts = vertices)
+                fixed (float* verts = vertices.ElementAt(bufferNum))
                 {
                     var ptr = new IntPtr(verts);
-                    gl.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices.Length * sizeof(float), ptr, OpenGL.GL_STATIC_DRAW);
+                    gl.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices.ElementAt(bufferNum).Length * sizeof(float), ptr, OpenGL.GL_STATIC_DRAW);
                 }
             }
         }
